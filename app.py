85699eaa2a8f3c42718293c9a3693bf7c6a0d95c
maureen <maureen.chelangat@student.moringaschool.com>
2025-03-21 11:40:03 +0300

    import policies resource and add resources to the api

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models import db, policy
+from routes import Policies, PolicyByID
+from flask_restful import Api
 
 app=Flask(__name__)
 
@@ -10,9 +12,14 @@ app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///policies.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
 
-migrate=Migrate(app, db)
 # Initialize the sqlalchemy instance with flask app
 db.init_app(app)
+migrate=Migrate(app, db)
+
+api=Api(app)
+
+api.add_resource(Policies, '/policies')   
+api.add_resource(PolicyByID, '/policies/<int:id>')
 
 # Ensure that the flask application only runs when the script is executed directly
 if __name__=='__main__':
