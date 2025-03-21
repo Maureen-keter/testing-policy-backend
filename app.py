dceb8cae0acb6b3485620e110ee1e2c8913617a7
maureen <maureen.chelangat@student.moringaschool.com>
2025-03-21 11:05:24 +0300

    create the table columns

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ db=SQLAlchemy()
 
 class Policy(db.Model, SerializerMixin)
     __tablename__='policies'
-    id = db.Column(db.Integer, primary_key=True)  # Primary key
-    name = db.Column(db.String(100), nullable=False)  # Policy name
-    type = db.Column(db.String(50), nullable=False)  # Policy type
-    premium = db.Column(db.Float, nullable=False)  # Premium amount
+    id = db.Column(db.Integer, primary_key=True)  
+    name = db.Column(db.String(100), nullable=False) 
+    type = db.Column(db.String(50), nullable=False) 
+    premium = db.Column(db.Float, nullable=False)  
     status = db.Column(db.String(20), default="active")
