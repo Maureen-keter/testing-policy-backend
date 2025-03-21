dc659dd6c2e3be266bd844e83e3bb27327c44a88
maureen <maureen.chelangat@student.moringaschool.com>
2025-03-21 11:03:12 +0300

    create a policy table

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+
+db=SQLAlchemy()
+
+
+class Policy(db.Model, SerializerMixin)
+    __tablename__='policies'
