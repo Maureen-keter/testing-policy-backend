ed8a606e4b363ac1dc4b0974bb53bdc6a3af8d87
maureen <maureen.chelangat@student.moringaschool.com>
2025-03-21 11:15:26 +0300

    configure the database

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ from flask_migrate import Migrate
 from models import db, policy
 
 app=Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///policies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
