8fca2353c4328a7676052efdc79f981cbeda0465
maureen <maureen.chelangat@student.moringaschool.com>
2025-03-21 11:56:32 +0300

    enable resource sharing using cors

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate
 from models import db
 from routes import Policies, PolicyByID
 from flask_restful import Api
-
+from flask_cors import CORS
 
 app=Flask(__name__)
 
@@ -15,6 +15,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 # Initialize the sqlalchemy instance with flask app
 db.init_app(app)
 migrate=Migrate(app, db)
+CORS(app)
 
 api=Api(app)
 
